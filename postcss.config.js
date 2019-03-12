7132b9cdf3530ff2fb1d0f36574b46756ab21595
Jeremy Wagner <jeremy.l.wagner@gmail.com>
2019-03-11 20:46:38 -0500

    step-0

--- postcss.config.js
+++ postcss.config.js
@@ -4,7 +4,7 @@ const devMode = process.env.NODE_ENV !== "production";
 
 let plugins = [
   require("autoprefixer")({
-    browsers: ["last 2 versions", "> 0.25%", "IE >= 11", "iOS >= 9", "Firefox ESR", "not dead"]
+    browsers: ["> 0.25%", "last 2 versions", "Firefox ESR", "not dead"]
   })
 ];
 
